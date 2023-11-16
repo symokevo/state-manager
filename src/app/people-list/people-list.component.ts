7ca84fb2b00293defb48d4225d1c0dde6c9fec0f
Simon Rabuogi <symokevo@gmail.com>
2023-11-17 01:28:31 +0300

    implemet people list form

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-people-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './people-list.component.html',
   styleUrls: ['./people-list.component.scss']
 })
 export class PeopleListComponent {
-
+  public nameFilter = new FormControl("");
+  // FormControl -> angular representation of control in a form.
+  // eg minlength, starting values, valid entries an so on. bridging html and angular implementation 
+  public input$ = this.nameFilter.valueChanges.pipe(
+    tap(
+      (f) => console.log(f)
+    )
+  );
+  // valueChanges - observer to take note of changes
 }
