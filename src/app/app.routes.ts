8099bcb590d94578e63ff4d69c44ff2bc1fe0bd8
Simon Rabuogi <symokevo@gmail.com>
2023-11-16 16:45:23 +0300

    Another commit. - I don't know which

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PeopleListComponent } from './people-list/people-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'people', pathMatch: 'full'},
+    { path: 'people', component: PeopleListComponent },
+];
---
+++ src/app/people-list/people-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-people-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './people-list.component.html',
+  styleUrls: ['./people-list.component.scss']
+})
+export class PeopleListComponent {
+
+}
