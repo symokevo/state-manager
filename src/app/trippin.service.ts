ab965951151d2a6274964118b41ed94e3c5ecf0a
Simon Rabuogi <symokevo@gmail.com>
2023-11-17 02:31:27 +0300

    Inject the baseUrl configuration to the trippin service

--- src/app/trippin.service.ts
+++ src/app/trippin.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { TRIPPIN_BASE_URL } from './app.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrippinService {
 
-  constructor() { }
+  constructor(@Inject(TRIPPIN_BASE_URL) private baseUrl: string) { }
 
   public getPeople(nameFilter?: string) : Observable<any[]> {
     // Access the Trippin OData api here
